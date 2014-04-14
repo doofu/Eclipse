2c428b8c6253c55c0103376b6ab99d056e2e7a4d
root <root@26c28e82-64b7-cd4d-bec5-8078ec826e7c>
2014-04-14 07:49:55 +0000

    实现Maven管理的三个骨架： pom-parent:实现父pom.xml，其他Maven的pom.xml可以继承该配置。 maven-quickStart:Maven的quick start骨架，继承了pom-parent的pom.xml文件 maven-webapp:Maven的webapp骨架，继承了pom-parent的pom.xml文件
    git-svn-id: svn://localhost/Eclipse@25 26c28e82-64b7-cd4d-bec5-8078ec826e7c

---
+++ maven-quickStart/src/main/java/com/zgz/it/maven_quickStart/App.java
@@ -0,0 +1,13 @@
+package com.zgz.it.maven_quickStart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pom-parent/src/main/java/com/zgz/it/pom_parent/App.java
@@ -0,0 +1,13 @@
+package com.zgz.it.pom_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
